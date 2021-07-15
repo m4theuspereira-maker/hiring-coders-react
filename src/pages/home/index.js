d0bf1d38005e5b14c041134d871dbbf70cf11d68
m4theuspereira-maker <matheusmonaco1@hotmail.com>
2021-07-15 17:29:23 -0300

    feat: add useEffect and home styling

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,20 +2,43 @@
 import React, { useState } from 'react'
 import * as S from './styled'
 import axios from 'axios'
+import { useHistory } from 'react-router-dom'
 
 function App(props) {
     const [usuario, setUsuario] = useState('')
+    const [error, setError] = useState(false)
+    const history = useHistory()
+
 
     const handlerUser = () => {
-        axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data))
+        axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
+
+            const repositories = response.data;
+
+            const repositoriesName = []
+            repositories.map((repo) => {
+                return repositoriesName.push(repo.name)
+            })
+            localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName))
+            history.push('/repositories')
+        }).catch(err => {
+            setError(true)
+        })
     }
     return (
-        <> <S.CONTAINER>
-            <p>{usuario}</p>
-            <h1>{props.title}</h1>
-            <S.INPUT name='usuario' id='usuario' className='usuarioInput' placeholder='user name' value={usuario} onChange={e => setUsuario(e.target.value)} />
-            <S.BUTTON type='buttom' onClick={handlerUser}>search</S.BUTTON>
-        </S.CONTAINER>
+        <>
+            <S.HOME_CONTAINER>
+                <S.CONTENT>
+                    <p>{usuario}</p>
+                    <h1>{props.title}</h1>
+                    <S.INPUT name='usuario' id='usuario' className='usuarioInput' placeholder='user name' value={usuario} onChange={e => setUsuario(e.target.value)} />
+                    <S.BUTTON type='buttom' onClick={handlerUser}>search</S.BUTTON>
+                </S.CONTENT>
+                {
+                    error ? <S.ERROR_MSG>Ocorreu um erro</S.ERROR_MSG> : ''
+                }
+            </S.HOME_CONTAINER>
+
         </>
     );
 }
